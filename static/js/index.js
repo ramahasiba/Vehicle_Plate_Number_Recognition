0163124f38c1e19c68d5e2a9880fba9ab69929be
Zubaida <sdrzubaida@gmail.com>
2023-03-16 15:18:11 +0200

    fetching response before merging

--- static/js/index.js
+++ static/js/index.js
@@ -30,23 +30,67 @@ var predictPlateNumber = function() {
 	alert('Plate number prediction is not implemented yet!');
 };
 
-async function submit(){
-	const image = document.getElementById('image');
-	const res = await fetch("/extract_number");
+// async function submit(){
+// 	const image = document.getElementById('image');
+// 	const res = await fetch("/extract_number");
 
-	if (res.ok){
-		const data = await res.json();
-		const extractNumber = data.extractNumber;
+// 	if (res.ok){
+// 		const data = await res.json();
+// 		const extractNumber = data.extractNumber;
 
-		// change the src of the img
-		image.src = data.imgsrc; 
-		// display the extracted number 
-		console.log("Extracted Number:",extractNumber);
-		// return the status of the operation
-		return {"Success": true, "Status": res.status};
-	}
-	else{
-		console.error("Status:", res.status)
-		return {"Success": false, "Status":res.status};
-	}
-}
+// 		// change the src of the img
+// 		image.src = data.imgsrc; 
+// 		// display the extracted number 
+// 		console.log("Extracted Number:",extractNumber);
+// 		// return the status of the operation
+// 		return {"Success": true, "Status": res.status};
+// 	}
+// 	else{
+// 		console.error("Status:", res.status)
+// 		return {"Success": false, "Status":res.status};
+// 	}
+// }
+
+async function get_num() {
+	let base64String = "";
+	var file = document.querySelector("input[type=file]")["files"][0];
+  
+	var reader = new FileReader();
+  
+	reader.onload = function () {
+	  base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+  
+	  imageBase64Stringsep = base64String;
+	  console.log(base64String);
+  
+	  // Convert the base64 string to a byte array
+	  var byteArray = new Uint8Array(atob(base64String).split("").map(function (c) { return c.charCodeAt(0); }));
+  
+	  // Convert the byte array to an image
+	  var image = byteArrayToImage(byteArray);
+  
+	  // Append the image to the document body
+	  document.body.appendChild(image);
+  
+	  // alert(imageBase64Stringsep);
+	};
+  
+	const formm = document.getElementById("forr");
+	// const submitter = document.querySelector("button[value=save]");
+	const formData = new FormData(formm);
+  
+	fetch("/uploadfile", {
+	  method: "POST",
+	  body: formData,
+	})
+	  .then((response) => response.json())
+	  .then((result) => {
+		console.log("Success:", result);
+	  })
+	  .catch((error) => {
+		console.error("Error:", error);
+	  });
+	  reader.readAsDataURL(file);
+  }
+  
+  
